18c21e12a17084015735893795fd56ad91a2437f
mdemrich94 <112007170+mdemrich94@users.noreply.github.com>
2025-04-23 23:01:07 -0600

    Update entrypoint.sh

--- part2/r3/entrypoint.sh
+++ part2/r3/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 ip addr flush dev eth0
-ip addr add 10.0.18.20/24 dev eth0
+ip addr add 10.0.15.10/24 dev eth0
 ip link set eth0 up
 
 ip addr flush dev eth1
-ip addr add 10.0.19.20/24 dev eth1
+ip addr add 10.0.18.20/24 dev eth1
 ip link set eth1 up
 
 ip addr flush dev eth2
-ip addr add 10.0.15.10/24 dev eth2
+ip addr add 10.0.19.20/24 dev eth2
 ip link set eth2 up
 
 /etc/init.d/frr start
